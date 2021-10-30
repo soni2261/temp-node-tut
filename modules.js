28159631965e7aa3d8adfe8978292af26660c715
soni2261 <soniarahal12@gmail.com>
2021-10-30 17:04:34 -0400

    first commit

---
+++ modules.js
@@ -0,0 +1,7 @@
+const names = require('./names');
+const sayHi = require('./function')
+const data = require('./alternative')
+require ('./mindgrenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
